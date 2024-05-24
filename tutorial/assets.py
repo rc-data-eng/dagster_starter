40cba700e8f7d978f7ca0cc7f513b8f67851f108
Sam Domingo <xanxussam@gmail.com>
2024-05-24 14:55:35 +0800

    test-python

--- tutorial/assets.py
+++ tutorial/assets.py
@@ -1,1 +1,12 @@
 
+import json
+import os
+
+import requests
+
+newstories_url = "https://hacker-news.firebaseio.com/v0/topstories.json"
+top_new_story_ids = requests.get(newstories_url).json()[:100]
+
+os.makedirs("data", exist_ok=True)
+with open("data/topstory_ids.json", "w") as f:
+    json.dump(top_new_story_ids, f)
