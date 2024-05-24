4e4826a9ea2e9e8d4a6b7d864270c631ef9ff6fe
Sam Domingo <xanxussam@gmail.com>
2024-05-24 15:34:23 +0800

    test1

--- tutorial/assets.py
+++ tutorial/assets.py
@@ -1,28 +1,14 @@
 import json
 import os
 
-import pandas as pd  # Add new imports to the top of `assets.py`
 import requests
+from dagster import asset # import the `dagster` library
 
-from dagster import asset
+@asset # add the asset decorator to tell Dagster this is an asset
+def topstory_ids() -> None:
+    newstories_url = "https://hacker-news.firebaseio.com/v0/topstories.json"
+    top_new_story_ids = requests.get(newstories_url).json()[:100]
 
-# ... Keep the `topstory_ids` asset from the previous section
-
-
-@asset(deps=[topstory_ids])  # this asset is dependent on topstory_ids
-def topstories() -> None:
-    with open("data/topstory_ids.json", "r") as f:
-        topstory_ids = json.load(f)
-
-    results = []
-    for item_id in topstory_ids:
-        item = requests.get(
-            f"https://hacker-news.firebaseio.com/v0/item/{item_id}.json"
-        ).json()
-        results.append(item)
-
-        if len(results) % 20 == 0:
-            print(f"Got {len(results)} items so far.")
-
-    df = pd.DataFrame(results)
-    df.to_csv("data/topstories.csv")
+    os.makedirs("data", exist_ok=True)
+    with open("data/topstory_ids.json", "w") as f:
+        json.dump(top_new_story_ids, f)
